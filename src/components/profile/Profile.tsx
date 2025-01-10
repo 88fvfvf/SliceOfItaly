9086ba5545a74e989fa7a21c01ab32058bfb1080
Humoyun Toshmatov <tosmatovhumoun03@gmail.com>
2025-01-10 19:02:37 +0500

    fix loading bag

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { ContextFirebase } from '../../main';
 import Header from '../Header/Header';
 import './Profile.scss';
@@ -13,19 +12,13 @@ const Profile = () => {
         console.error('Firebase context is not available');
         return null; // Можно вернуть null, если контекст не доступен
     }
-    const { auth } = firebaseContext; // получаем объект auth из контекста
-    const [loading] = useAuthState(auth); // хук, который возвращает пользователя, загрузку и ошибку
 
     return (
         <div className='profile'>
             <Header />
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                <>
-                    <TabsProfile />
-                </>
-            )}
+            <>
+                <TabsProfile />
+            </>
         </div>
     );
 };
